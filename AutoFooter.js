1e80c36b89fdd89a7d8a81eb9e285f11193caf5a
GDS <50288897+GDS-Engineering@users.noreply.github.com>
2019-05-05 16:31:29 +0200

    Polishing

--- AutoFooter.js
+++ AutoFooter.js
@@ -1,4 +1,4 @@
-// Settings for AutoFooter.
+// Settings for AutoFooter
 var fromYear            = 2001;
 var showRange           = true;
 var showMiddleChar      = true;
@@ -9,5 +9,5 @@ var showTime            = true;
 var parseAsParagraph    = false;
 var finalString         = "AutoFooter Inc. All rights reserved.";
 
-
+// Script body
 var currentYear=(new Date).getFullYear(),cc="",space=" ";if(showCopyright)switch(copyrightFormat){case 0:cc="©";break;case 1:cc="Copyright";break;case 2:cc="Copyright ©";break;case 3:cc="© Copyright";break;default:cc="©"}var cr=showCopyright?cc+space:"";if(showTime)if(showRange)if(currentYear>fromYear){var mc=showMiddleChar?middleChar:"";out(cr+fromYear+space+mc+space+currentYear)}else out(cr+currentYear);else out(cr+currentYear);else out(cr);function out(r){r=r+space+finalString,parseAsParagraph?document.write("<p>"+r+"</p>"):document.write(r)}
