1ecdb3100cf9a7aa87cfed977d4fd979358957f1
GDS <50288897+GDS-Engineering@users.noreply.github.com>
2019-05-05 10:52:44 +0200

    Minified script

--- AutoFooter.js
+++ AutoFooter.js
@@ -10,45 +10,4 @@ var parseAsParagraph    = false;
 var finalString         = "AutoFooter Inc. All rights reserved.";
 
 
-
-var currentYear = new Date().getFullYear();
-var cc = "";
-var space = " ";
-if (showCopyright) {
-    switch (copyrightFormat) {
-        case 0:
-            cc = "©";
-            break;
-        case 1:
-            cc = "Copyright";
-            break;
-        case 2:
-            cc = "Copyright ©";
-            break;
-        case 3:
-            cc = "© Copyright";
-            break;
-        default:
-            cc = "©";
-            break;
-    }
-}
-
-var cr = showCopyright ? cc + space : "";
-if (showTime) {
-    if (showRange) {
-        if (currentYear > fromYear) {
-            var mc = showMiddleChar ? middleChar : "";
-            out(cr + fromYear + space + mc + space + currentYear);
-        } else out(cr + currentYear);
-    } else out(cr + currentYear);
-} else {
-    out(cr);
-}
-
-function out(str) {
-    str = str + space + finalString;
-    if (parseAsParagraph) {
-        document.write("<p>" + str + "</p>");
-    } else document.write(str)
-}
+var currentYear=(new Date).getFullYear(),cc="",space=" ";if(showCopyright)switch(copyrightFormat){case 0:cc="©";break;case 1:cc="Copyright";break;case 2:cc="Copyright ©";break;case 3:cc="© Copyright";break;default:cc="©"}var cr=showCopyright?cc+space:"";if(showTime)if(showRange)if(currentYear>fromYear){var mc=showMiddleChar?middleChar:"";out(cr+fromYear+space+mc+space+currentYear)}else out(cr+currentYear);else out(cr+currentYear);else out(cr);function out(r){r=r+space+finalString,parseAsParagraph?document.write("<p>"+r+"</p>"):document.write(r)}
