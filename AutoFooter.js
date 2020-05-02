e67a1040d94d48ab1de534a80423e393cc8ab62d
GDS <50288897+GDS98@users.noreply.github.com>
2020-05-02 18:20:40 +0200

    Updated variable declaration

--- AutoFooter.js
+++ AutoFooter.js
@@ -1,13 +1,12 @@
 // Settings for AutoFooter
-var fromYear            = 2001;
-var showRange           = true;
-var showMiddleChar      = true;
-var middleChar          = "-";
-var showCopyright       = true;
-var copyrightFormat     = 2;
-var showTime            = true;
-var parseAsParagraph    = false;
-var finalString         = "AutoFooter Inc. All rights reserved.";
+const fromYear            = 2001;
+const showRange           = true;
+const showMiddleChar      = true;
+const middleChar          = "-";
+const showCopyright       = true;
+const copyrightFormat     = 2;
+const showTime            = true;
+const parseAsParagraph    = false;
+const finalString         = "AutoFooter Inc. All rights reserved.";
 
-// Script body
 var currentYear=(new Date).getFullYear(),cc="",space=" ";if(showCopyright)switch(copyrightFormat){case 0:cc="©";break;case 1:cc="Copyright";break;case 2:cc="Copyright ©";break;case 3:cc="© Copyright";break;default:cc="©"}var cr=showCopyright?cc+space:"";if(showTime)if(showRange)if(currentYear>fromYear){var mc=showMiddleChar?middleChar:"";out(cr+fromYear+space+mc+space+currentYear)}else out(cr+currentYear);else out(cr+currentYear);else out(cr);function out(r){r=r+space+finalString,parseAsParagraph?document.write("<p>"+r+"</p>"):document.write(r)}
